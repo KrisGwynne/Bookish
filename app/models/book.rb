6670f99759f0a4dcdfaaca5713491d32b29a62be
Kris Gwynne <Kris.Gwynne@softwire.com>
2020-09-10 15:13:29 +0100

    Added foreign key to book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,4 @@
 class Book < ApplicationRecord
-  has_many :copies, dependent: :destroy
+  has_many :copies, foreign_key: :book_id, dependent: :destroy
   validates :Title, :Author, :ISBN, presence: true
 end
