1d087bfbe05b814927dd9c99baa4cb2659a00930
Kris Gwynne <Kris.Gwynne@softwire.com>
2020-09-10 18:07:50 +0100

    Added foreign key migration for copies

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_10_140347) do
+ActiveRecord::Schema.define(version: 2020_09_10_170221) do
 
   create_table "books", force: :cascade do |t|
     t.datetime "created_at", null: false
